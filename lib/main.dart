03de12a13ccc2a3ccaed4eecd1a12fa54dce741f
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2023-07-08 12:59:56 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'todo_provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => TodoProvider(),
+      child: MaterialApp(
+        title: 'Todo App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: TodoListScreen(),
+      ),
+    );
+  }
+}
+
+class TodoListScreen extends StatefulWidget {
+  @override
+  _TodoListScreenState createState() => _TodoListScreenState();
+}
+
+class _TodoListScreenState extends State<TodoListScreen> {
+  final TextEditingController _titleController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<TodoProvider>(context, listen: false).fetchTodos();
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final todoProvider = Provider.of<TodoProvider>(context);
+    final todos = todoProvider.todos;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo List'),
+      ),
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          final todo = todos[index];
+          _titleController.text = todo.title;
+
+          return Dismissible(
+            key: Key(todo.id.toString()),
+            background: Container(
+              color: Colors.red,
+              child: Icon(
+                Icons.delete,
+                color: Colors.white,
+              ),
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.only(right: 16.0),
+            ),
+            direction: DismissDirection.endToStart,
+            onDismissed: (direction) async {
+              await todoProvider.deleteTodo(
+                todoId: todo.id,
+              );
+            },
+            child: ListTile(
+              title: Text(todo.title),
+              subtitle: Text(todo.completed ? 'Completed' : 'Not Completed'),
+              trailing: Checkbox(
+                value: todo.completed,
+                onChanged: (value) async {
+                  await todoProvider.updateTodo(
+                    todoId: todo.id,
+                    title: todo.title,
+                    completed: value ?? false,
+                  );
+                },
+              ),
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text('Edit Todo'),
+                      content: TextField(
+                        controller: _titleController,
+                        decoration: InputDecoration(labelText: 'Title'),
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('Cancel'),
+                        ),
+                        TextButton(
+                          onPressed: () async {
+                            String title = _titleController.text;
+                            if (title.isNotEmpty) {
+                              await todoProvider.updateTodo(
+                                todoId: todo.id,
+                                title: title,
+                                completed: todo.completed,
+                              );
+                              Navigator.of(context).pop();
+                            }
+                          },
+                          child: Text('Save'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text('Add Todo'),
+                content: TextField(
+                  controller: _titleController,
+                  decoration: InputDecoration(labelText: 'Title'),
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('Cancel'),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      String title = _titleController.text;
+                      if (title.isNotEmpty) {
+                        await todoProvider.createTodo(
+                          title: title,
+                        );
+                        Navigator.of(context).pop();
+                      }
+                    },
+                    child: Text('Add'),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/todo_provider.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/foundation.dart';
+import 'package:dio/dio.dart';
+
+class Todo {
+  final int id;
+  String title;
+  bool completed;
+
+  Todo({
+    required this.id,
+    required this.title,
+    required this.completed,
+  });
+
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    return Todo(
+      id: json['id'],
+      title: json['title'],
+      completed: json['completed'],
+    );
+  }
+}
+
+class TodoProvider extends ChangeNotifier {
+  final String apiUrl =
+      'http://localhost:8000/api'; // Replace with your Laravel API URL
+  late Dio _dio;
+
+  List<Todo> _todos = [];
+  List<Todo> get todos => _todos;
+
+  TodoProvider() {
+    _dio = Dio();
+  }
+
+  Future<void> fetchTodos() async {
+    try {
+      final response = await _dio.get('$apiUrl/todos');
+
+      if (response.statusCode == 200) {
+        final List<dynamic> responseData = response.data;
+        _todos = responseData.map((item) => Todo.fromJson(item)).toList();
+      } else {
+        throw Exception('Failed to fetch todos');
+      }
+    } catch (error) {
+      throw Exception('Failed to fetch todos');
+    }
+    notifyListeners();
+  }
+
+  Future<void> createTodo({
+    required String title,
+  }) async {
+    try {
+      final response = await _dio.post('$apiUrl/todos', data: {'title': title});
+
+      if (response.statusCode == 201) {
+        final dynamic responseData = response.data;
+        Todo newTodo = Todo.fromJson(responseData);
+        _todos.add(newTodo);
+      } else {
+        throw Exception('Failed to create todo');
+      }
+    } catch (error) {
+      throw Exception('Failed to create todo');
+    }
+    notifyListeners();
+  }
+
+  Future<void> updateTodo({
+    required int todoId,
+    required String title,
+    required bool completed,
+  }) async {
+    try {
+      final response = await _dio.put('$apiUrl/todos/$todoId',
+          data: {'title': title, 'completed': completed.toString()});
+
+      if (response.statusCode == 200) {
+        final dynamic responseData = response.data;
+        Todo updatedTodo = Todo.fromJson(responseData);
+        int index = _todos.indexWhere((todo) => todo.id == updatedTodo.id);
+        if (index != -1) {
+          _todos[index] = updatedTodo;
+        }
+      } else {
+        throw Exception('Failed to update todo');
+      }
+    } catch (error) {
+      throw Exception('Failed to update todo');
+    }
+    notifyListeners();
+  }
+
+  Future<void> deleteTodo({
+    required int todoId,
+  }) async {
+    try {
+      final response = await _dio.delete('$apiUrl/todos/$todoId');
+
+      if (response.statusCode == 200) {
+        _todos.removeWhere((todo) => todo.id == todoId);
+      } else {
+        throw Exception('Failed to delete todo');
+      }
+    } catch (error) {
+      throw Exception('Failed to delete todo');
+    }
+    notifyListeners();
+  }
+}
